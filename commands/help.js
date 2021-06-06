9f6bb4317a83eea980e072cf7a87f5b68ddf7f20
Melissa20XX <65194138+Melissa20XX@users.noreply.github.com>
2021-06-06 15:27:19 -0600

    Reformatting and Commenting

--- commands/help.js
+++ commands/help.js
@@ -5,42 +5,60 @@ module.exports = {
 	description: 'Lists all commands, or gives information on a specific command',
 	cooldown: 5,
 	usage: '<command name>',
+	
 	execute(message, args){
 		const data = []
 		const { commands } = message.client
 
-		if(!args.length){
-data.push('Here\'s a list of all my commands:');
-data.push(commands.map(command => command.name).join(', '));
-data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+		//data.push is used to append shit to the message that is sent at command end
 
-return message.author.send(data, { split: true })
-	.then(() => {
-		if (message.channel.type === 'dm') return;
-		message.reply('I\'ve sent you a DM with all my commands!');
-	})
-	.catch(error => {
-		console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-		message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-	});
+		//If the arguments list is empty, just list all the commands
+		if(!args.length) 
+		{
+			data.push('Here\'s a list of all my commands:');
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+
+			//DM pushed data to author of original message
+			return message.author.send(data, { split: true })
+			
+			.then( () => {
+			//bot doesnt say this if command was sent via dm
+			if (message.channel.type === 'dm') return;
+			message.reply('I\'ve sent you a DM with all my commands!');
+			})
+			
+			.catch(error => {
+			console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+			message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+			});
 		}
 
-const name = args[0].toLowerCase();
-const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+		//var name is the 1st argument and is the name of the desired command
+		const name = args[0].toLowerCase();
+		//var name is then used to attach a relevant file to var command
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
-if (!command) {
-	return message.reply('that\'s not a valid command!');
-}
+		//if no valid command file was found then u done goofed & we return
+		if (!command) {
+			return message.reply('lol i cant give info about an invalid command');
+		}
 
-data.push(`**Name:** ${command.name}`);
+		//if we made it this far, they want command metadata, and no data has been pushed yet
+		data.push(`**Name:** ${command.name}`);
 
-if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-if (command.description) data.push(`**Description:** ${command.description}`);
-if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		//only display metadata that exists
+		if (command.aliases)
+			{ data.push(`**Aliases:** ${command.aliases.join(', ')}`); }
+		if (command.description)
+			{ data.push(`**Description:** ${command.description}`); }
+		if (command.usage)
+			{ data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`); }
 
-data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+		//every command has a cooldown, undefined = 3 seconds
+		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
-message.channel.send(data, { split: true });
+		message.channel.send(data, { split: true });
 	
 	},
 };
